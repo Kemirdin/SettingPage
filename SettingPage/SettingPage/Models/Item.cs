dda93144f3d7cf0415bdf808433a1bc81e855184
Kemirdin Kaynam <qemirdin@gmail.com>
2020-05-20 21:41:43 +0200

    Add project files.

---
+++ SettingPage/SettingPage/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SettingPage.Models
+{
+	public class Item
+	{
+		public string Id { get; set; }
+		public string Text { get; set; }
+		public string Description { get; set; }
+	}
+}
